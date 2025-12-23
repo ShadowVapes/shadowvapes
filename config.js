2b2299d28f92583f48dce4bc9f47404ebbd5be6b
Shadow01 <68017917+AdriVaok55@users.noreply.github.com>
2025-12-23 02:02:50 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// ÁLLÍTSD BE EZT A 3 SORT:
+export const GITHUB_OWNER = "AdriVaok55";
+export const GITHUB_REPO  = "shadowvapes";
+export const GITHUB_BRANCH = "main";
+
+// fájl, amibe mentünk:
+export const DATA_PATH = "data/products.json";
+
+// Admin "jelszó" (gyors megoldás, nem enterprise security)
+export const ADMIN_PASSWORD = "valami_eros_jelszo_123";
